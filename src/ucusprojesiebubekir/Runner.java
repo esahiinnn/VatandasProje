23a5056c834e197381de31f8546511728fb4c35c
Ebubekir <esahin3@icloud.com>
2021-05-08 01:03:35 +0300

    Ucus projesi eklendi

---
+++ src/ucusprojesiebubekir/Runner.java
@@ -0,0 +1,10 @@
+package ucusprojesiebubekir;
+
+public class Runner  {
+    public static void main(String[] args) {
+        UcusBilgileri ucus = new UcusBilgileri();
+        System.out.println("Ucus firmamiza hosgeldiniz!");
+        ucus.menu();
+
+    }
+}
